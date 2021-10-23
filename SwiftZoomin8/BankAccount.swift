1c0be717534916335ad6ef780131f684b4af0a82
dsxs <dsxsxsxs@gmail.com>
2021-10-23 22:44:47 +0900

    calc interest

--- SwiftZoomin8/BankAccount.swift
+++ SwiftZoomin8/BankAccount.swift
@@ -2,6 +2,11 @@ import Foundation
 
 actor BankAccount {
     private var balance: Int = 0
+
+    func getInterest(with rate: Double) -> Int {
+        let interest = Double(balance) * rate
+        return deposit(amount: Int(interest))
+    }
     func deposit(amount: Int)  -> Int {
         balance += amount
         return balance
@@ -12,10 +17,8 @@ func bankAccountMain() {
     let account = BankAccount()
     Task {
         print(await account.deposit(amount: 1000))
-    }
-
-    Task {
         print(await account.deposit(amount: 1000))
+        print(await account.getInterest(with: 0.05))
     }
 
 }
