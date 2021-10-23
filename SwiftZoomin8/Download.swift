f772088a46a06fd1fec797e6c29fcb5868e32133
dsxs <dsxsxsxs@gmail.com>
2021-10-23 21:31:25 +0900

    only data

--- SwiftZoomin8/Download.swift
+++ SwiftZoomin8/Download.swift
@@ -21,12 +21,12 @@ func downloadData(from url: URL, completion: @escaping (Result<Data, Error>) -> 
 }
 
 
-func downloadData(from url: URL) async throws -> (Data, URLResponse) {
+func downloadData(from url: URL) async throws -> Data {
     let (data, response) = try await URLSession.shared.data(from: url)
     if let response = response as? HTTPURLResponse {
         guard response.statusCode == 200 else {
             throw ResponseError(statusCode: response.statusCode)
         }
     }
-    return (data, response)
+    return data
 }
