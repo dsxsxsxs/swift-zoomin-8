e820d3b586c51427b7dd4d75f9c47658ab176be6
dsxs <dsxsxsxs@gmail.com>
2021-10-23 22:16:20 +0900

    actor demo

--- SwiftZoomin8/BankAccount.swift
+++ SwiftZoomin8/BankAccount.swift
@@ -1,2 +1,21 @@
+import Foundation
+
+actor BankAccount {
+    private var balance: Int = 0
+    func deposit(amount: Int)  -> Int {
+        balance += amount
+        return balance
+    }
+}
+
 func bankAccountMain() {
+    let account = BankAccount()
+    Task {
+        print(await account.deposit(amount: 1000))
+    }
+
+    Task {
+        print(await account.deposit(amount: 1000))
+    }
+
 }
